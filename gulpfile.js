a289ccefb79ec495426b4d2f665df7d97479e9e0
John Erickson <john.t.erickson@gmail.com>
2019-12-27 21:41:55 -0800

    fixed by copying morefoods

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
 const MinecraftAddonBuilder = require("minecraft-addon-toolchain/v1");
-const BrowserifySupport = require("minecraft-addon-toolchain-browserify");
 
-const builder = new MinecraftAddonBuilder("Ice Cream Pack");
-builder.addPlugin(new BrowserifySupport());
+const builder = new MinecraftAddonBuilder("IceCreamPack");
 
 module.exports = builder.configureEverythingForMe();
